926fde33a953ee94de981667ccb924c24c6a8739
josuf107 <joseph.m.barratt@gmail.com>
2014-12-08 17:13:18 -0600

    Initial implementation of findMissing #2

--- Resolver.hs
+++ Resolver.hs
@@ -2,6 +2,7 @@ import qualified Data.Csv as Csv
 import qualified Data.ByteString.Lazy as BS
 import qualified Data.Text as Text
 import qualified Data.Vector as Vector
+import qualified Data.Set as Set
 import Data.Maybe
 import Fizz.Core
 import Fizz.Store
@@ -20,7 +21,16 @@ data BankEntry
     } deriving (Show, Eq)
 
 findMissing :: Journal -> [BankEntry] -> [BankEntry]
-findMissing j be = undefined
+findMissing j bes
+    = filter (notIn j)
+    $ bes
+    where
+        notIn j be =
+            case bankEntryType be of
+                Deposit -> not $ Set.member (bankEntryValue be) deposits
+                Withdrawal ->  not $ Set.member (bankEntryValue be) withdrawals
+        deposits = Set.fromList . fmap (\(Earn e) -> getExpenseValue e) . filter isEarn . fmap snd $ j
+        withdrawals = Set.fromList . fmap (\(Spend e) -> getExpenseValue e) . filter isSpend . fmap snd $ j
 
 parseFile :: FilePath -> IO (Either String [BankEntry])
 parseFile = fmap parseLines . BS.readFile
